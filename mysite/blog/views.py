fadc563cb16598d7c1a17257181253dfa5e7a0d7
UAustin <liuyinggang08@163.com>
2016-08-27 20:10:08 +0800

    update

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,7 @@ from .models import Post
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     object_list = Post.objects.filter(status="published")
@@ -44,8 +45,12 @@ def post_detail(request, year, month, day, post):
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.save()
-
-    return render(request, 'blog/post/detail.html', {'post':post, "comments":comments, "comment_form":comment_form,}) 
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-published')[:4]
+     
+    return render(request, 'blog/post/detail.html', {'post':post, "comments":comments, "comment_form":comment_form, "similar_posts":similar_posts}) 
     
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status="published")
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,12 +40,6 @@ INSTALLED_APPS = (
     'blog',
     'taggit',
 )
-
-from django.conf import global_settings
-TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
-    'django.core.context_processors.request',
-)
-BOOTSTRAP_ADMIN_SIDEBAR_MENU =True
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
