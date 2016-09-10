83d49a9a45c9e76eb71fadc6862d7914853774a0
UAustin <liuyinggang08@163.com>
2016-09-10 11:38:55 +0800

    update again

--- mysite/articles/models.py
+++ mysite/articles/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='article_created')
 	title = models.CharField(max_length=200)
 	article = models.TextField(blank=True)
-	created_time = models.DateField(auto_now_add=Truem, db_index=True)
+	created_time = models.DateField(auto_now_add=True, db_index=True)
 	users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='article_liked', blank=True)
 
 	class Meta:
--- mysite/articles/views.py
+++ mysite/articles/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from .models import Article
+from . forms import ArticleCreateForm
+from django.http import Http404
 
 # Create your views here.
+def list_articles(request):
+	my_articles = Article.objects.all()
+	return render(request, "article/list_articles.html", {"articles":my_articles})
+
+@login_required(login_url="/account/login/")
+def publish_article(request):
+	if request.method == "GET":
+		form = ArticleCreateForm()
+		return render(request, "article/publish_article.html", {"form":form})
+
+	if request.method == "POST":
+		form = ArticleCreateForm(data=request.POST)
+		if form.is_valid:
+			try:
+				new_item = form.save(commit=False)
+				new_item.user = request.user
+				new_item.save()
+				return redirect("articles:list_articles")
+			except:
+				raise Http404("Sorry! Input title and article, please.")
